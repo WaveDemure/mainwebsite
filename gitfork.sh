aa2ed3565a99520017d555eba05aa3c76844a908
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-10-11 23:44:24 -0500

    Update and rename s.sh to gitfork.sh

---
+++ gitfork.sh
@@ -0,0 +1,54 @@
+clear
+
+# TESTING
+
+if ! command -v gh >/dev/null 2>&1; then
+    echo "Install gh first"
+    sleep 2
+    exit 1
+fi
+
+if ! gh auth status >/dev/null 2>&1; then
+    echo "You need to login: gh auth login"
+    sleep 2
+    exit 1
+fi
+
+
+# the stuffs
+
+FAILED=false
+nameC=false
+skipName=false
+
+echo "Welcome to github overwrite forker"
+echo "(will on work on YOUR repositorys *nice try*)"
+read -p "[Link for repo to fork]#> " testie
+
+if [[ $testie == "deleteAll" ]]; then
+    echo "THIS WILL DELETE ALL FILES ON YOUR REPO"
+    testie="https://github.com/WaveDemure/mt.git"
+    skipName=true 
+    Nane="mt"
+    nameC=true
+fi
+
+if [[ $skipName != true ]]; then
+    read -p "[Name of the repo your forking]#> " Nane 
+    if echo $testie | grep $Nane; then
+        echo "FOUND NAME"
+        nameC=true
+    else
+        echo "ABORTING NAME NOT FOUND"
+    fi 
+fi
+
+if [[ $nameC == true ]]; then
+    git clone --bare -q $testie
+    cd $Nane.git
+    read -p "[Link to your repository]> " newmirror
+    git push --mirror $newmirror.git
+    cd -
+    echo "Done!"
+    rm -rf $Nane.git
+fi
